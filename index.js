7001fe39e9b7905793b340e3e40060518a272257
Controlfreak707 <controlfreak707@gmail.com>
2021-03-24 13:15:48 -0700

    Subcommands go brbrbrbrbr

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ export default class ExpandedEmoticons extends Plugin {
     Emoticons.forEach((emoticon) => {
       if (typeof emoticon.emoticon != "string") {
         const parentJson = emoticon;
+
+        let varients = [];
         emoticon.emoticon.forEach((emoticon) => {
-          Commands.registerCommand({
-            command:
-              parentJson.name + (parentJson.emoticon.indexOf(emoticon) + 1),
-            aliases: parentJson?.aliases,
+          varients.push({
+            command: `${parentJson.emoticon.indexOf(emoticon) + 1}`,
             description: `Appends ${emoticon} to your message.`,
             options: [
               {
@@ -28,6 +28,35 @@ export default class ExpandedEmoticons extends Plugin {
               };
             },
           });
+        });
+
+        Commands.registerCommand({
+          command: emoticon.name,
+          aliases: emoticon?.aliases,
+          description: "See subcommands for details.",
+          executor: (args) => {
+            const subcommand = varients[args[0] - 1];
+            if (!subcommand) {
+              return {
+                result: `Please specify a valid varient.`,
+              };
+            }
+
+            return subcommand.executor(args.slice(1));
+          },
+          autocomplete: (args) => {
+            if (args[0] != void 0 && args.length == 1) {
+              return {
+                commands: Object.values(varients).filter(({ command }) =>
+                  command.includes(args[0])
+                ),
+              };
+            }
+
+            if (!varients[args[0] - 1]) {
+              return false;
+            }
+          },
         });
       } else {
         Commands.registerCommand({
